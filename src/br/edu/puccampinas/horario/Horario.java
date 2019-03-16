baec4cf5d76fa09387acfd0e07cb7e27c0d2a52a
Aleph Santos Oliveira <aleph.oliveira@assertivasolucoes.com.br>
2019-03-16 18:24:56 -0300

    Adicionada suite de testes

---
+++ src/br/edu/puccampinas/horario/Horario.java
@@ -0,0 +1,116 @@
+package br.edu.puccampinas.horario;
+
+public class Horario {
+
+  private int horas, minutos, segundos;
+
+  public Horario(int horas, int minutos, int segundos) throws Exception {
+    valida(horas, minutos, segundos);
+    this.horas = horas;
+    this.minutos = minutos;
+    this.segundos = segundos;
+  }
+
+  private void valida(int horas, int minutos, int segundos) throws Exception {
+    if (horas < 0 || horas > 24) {
+      throw new Exception("Hora \"" + horas + "\" é inválida!");
+    }
+    if (minutos < 0 || minutos > 59) {
+      throw new Exception("Minutos \"" + minutos + "\" são inválidos!");
+    }
+    if (segundos < 0 || segundos > 59) {
+      throw new Exception("Segundos \"" + segundos + "\" são inválidos!");
+    }
+  }
+
+  public int getHoras() {
+    return this.horas;
+  }
+
+  public void setHoras(int horas) throws Exception {
+    valida(horas, this.minutos, this.segundos);
+    this.horas = horas;
+  }
+
+  public int getMinutos() {
+    return this.minutos;
+  }
+
+  public void setMinutos(int minutos) throws Exception {
+    valida(this.horas, minutos, this.segundos);
+    this.minutos = minutos;
+  }
+
+  public int getSegundos() {
+    return this.segundos;
+  }
+
+  public void setSegundos(int segundos) throws Exception {
+    valida(this.horas, this.minutos, segundos);
+    this.segundos = segundos;
+  }
+
+  private int emSegundos() {
+    return this.segundos + (this.minutos * 60) + (this.horas * 3600);
+  }
+
+  public static Horario segundosEmHorario(int qtdSegundos) throws Exception {
+    if (qtdSegundos < 0) {
+      throw new Exception("Quantidade de segundos inválida!");
+    }
+    int seg, min, h;
+	seg = qtdSegundos%60;
+	min = (qtdSegundos/60)%60;
+	h = (qtdSegundos/3600)%24;
+    return new Horario(h, min, seg);
+  }
+
+  public void adiante(int qtdSegundos) throws Exception {
+    if (qtdSegundos < 0) {
+      throw new Exception("Quantidade de segundos inválida!");
+    }
+    Horario h = segundosEmHorario(this.emSegundos() + qtdSegundos);
+    this.horas = h.getHoras();
+    this.minutos = h.getMinutos();
+    this.segundos = h.getSegundos();
+    // Implementação procedural
+    // this.segundos += qtdSegundos;
+    // this.minutos += this.segundos/60;
+    // this.segundos %= 60;
+    // this.horas += this.minutos/60;
+    // this.minutos %= 60;
+    // this.horas %= 24;
+  }
+
+  public void atrase(int qtdSegundos) throws Exception {
+    if (qtdSegundos < 0) {
+      throw new Exception("Quantidade de segundos inválida!");
+    }
+    int dia = 86400;
+    int atraso = this.emSegundos() - (qtdSegundos % dia);
+    atraso = atraso < 0 ? dia - atraso : atraso;
+    Horario h = segundosEmHorario(atraso);
+    this.horas = h.getHoras();
+    this.minutos = h.getMinutos();
+    this.segundos = h.getSegundos();
+    // Implementação procedural
+    // this.segundos -= qtdSegundos%60;
+    // this.segundos = this.segundos < 0 ? 60 + this.segundos : this.segundos;
+    // this.minutos -= (this.segundos + qtdSegundos)/60;
+    // this.minutos %= 60;
+    // this.minutos = this.minutos < 0 ? 60 + this.minutos : this.minutos;
+    // this.horas -= (this.minutos + ((this.segundos + qtdSegundos)/60))/60;
+    // this.horas %= 24;
+    // this.horas = this.horas < 0 ? 24 + this.horas : this.horas;
+  }
+
+
+  public String toString() {
+    return (horas < 10 ? "0" + horas : horas) 
+			+ ":" +
+			(minutos < 10 ? "0" + minutos : minutos) 
+			+ ":"+ 
+			(segundos < 10 ? "0" + segundos : segundos);
+  }
+
+}
---
+++ src/br/edu/puccampinas/horario/HorarioTest.java
@@ -0,0 +1,87 @@
+package br.edu.puccampinas.horario;
+
+import java.util.logging.Logger;
+/**
+ * Suíte de testes da classe Horário
+ * @author aleph
+ *
+ */
+public class HorarioTest {
+  private static final Logger LOGGER = Logger.getLogger(HorarioTest.class.getName());
+
+  /**
+   * Driver para iniciar suíte de testes
+   * @param args
+   */
+  public static void main(String[] args) {
+    try {
+      deve_impedir_horas_invalidas();
+      deve_impedir_minutos_invalidos();
+      deve_impedir_segundos_invalidos();
+      deve_adiar_horario();
+      deve_atrasar_horario();
+      deve_modificar_horario();
+    } catch (Exception erro) {
+      LOGGER.severe(erro.getMessage());
+    }
+  }
+
+  private static void deve_impedir_horas_invalidas() throws Exception {
+    try {
+      Horario h = new Horario(10000, 15, 58);
+      throw new Exception("Um horário com horas inválidas foi criado: " + h);
+    } catch (Exception e) {
+      LOGGER.info("Horas inválidas foram tratadas com sucesso.");
+    }
+  }
+  
+  private static void deve_impedir_minutos_invalidos() throws Exception {
+    try {
+      Horario h = new Horario(23, 10000, 58);
+      throw new Exception("Um horário com minutos inválidos foi criado: " + h);
+    } catch (Exception e) {
+      LOGGER.info("Minutos inválidos foram tratados com sucesso.");
+    }
+  }
+  
+  private static void deve_impedir_segundos_invalidos() throws Exception {
+    try {
+      Horario h = new Horario(23, 15, 10000);
+      throw new Exception("Um horário com segundos inválidos foi criado: " + h);
+    } catch (Exception e) {
+      LOGGER.info("Segundos inválidos foram tratados com sucesso.");
+    }
+  }
+
+  private static void deve_adiar_horario() throws Exception {
+    Horario h = new Horario(6,30,5);
+    h.adiante(3600 * 5 + 1800 - 5);
+    if (h.getHoras() == 12 && h.getMinutos() == 0 && h.getSegundos() == 0) {
+      LOGGER.info("Horário adiantado com sucesso.");
+      return;
+    }
+    throw new Exception("O horário não foi adiantado corretamente: " + h);
+  }
+  
+  private static void deve_atrasar_horario() throws Exception {
+    Horario h = new Horario(12,0,0);
+    h.atrase(400000);
+    if (h.getHoras() == 3 && h.getMinutos() == 6 && h.getSegundos() == 40) {
+      LOGGER.info("Horário atrasado com sucesso.");
+      return;
+    }
+    throw new Exception("O horário não foi atrasado corretamente: " + h);
+  }
+  
+  private static void deve_modificar_horario() throws Exception {
+    Horario h = new Horario(13, 15, 58);
+    h.setHoras(23);
+    h.setMinutos(17);
+    h.setSegundos(9);
+    if (h.getHoras() == 23 && h.getMinutos() == 17 && h.getSegundos() == 9) {
+      LOGGER.info("Horário modificado com sucesso.");
+      return;
+    }
+    throw new Exception("O horário não foi modificado corretamente: " + h);
+  }
+}
